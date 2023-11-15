a152b4ad79f9ca1539a316d713db476124c42e27
Md. Bulbul Ahmed <bulbulahmed.tk@gmail.com>
2023-11-15 22:45:49 +0600

    feat : added generate token method

--- Repository/User.cs
+++ Repository/User.cs
@@ -1,10 +1,41 @@
 
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Mvc;
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 public class User : IUser
 {
+
+    private readonly IConfiguration _configuration;
+
+    public User(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateJwtToken(string Email)
+    {
+        var audienceConfig = _configuration.GetSection("Audience");
+        var Secret = audienceConfig["Secret"] ?? "";
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Secret);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Email, Email),
+                new Claim("Name", "Md. Bulbul")
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
     public async Task<string> CreateUser()
     {
         try
@@ -17,15 +48,21 @@ public class User : IUser
             throw;
         }
     }
+
+
     public async Task<string> LoginUser(LoginUserDTO obj)
     {
         try
         {
-            if(string.IsNullOrEmpty(obj.Email) || string.IsNullOrEmpty(obj.Password))
+            if (string.IsNullOrEmpty(obj.Email) || string.IsNullOrEmpty(obj.Password))
             {
                 throw new Exception("Invalid Input");
             }
-            return default;
+
+            var token = GenerateJwtToken(obj.Email);
+
+            return token;
+
         }
         catch (System.Exception)
         {
